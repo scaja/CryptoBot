2b7b405bd282ffab670061a283d86132ce7458dc
Ubuntu <ubuntu@ip-172-31-32-192.eu-west-1.compute.internal>
2024-12-28 17:45:05 +0000

    write dataframe directly into elasticsearch

--- 02_data_understanding.py
+++ 02_data_understanding.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
-# CryptoBot - data understanding historical
+import bulk_script
 
-# 1. Import Libraries & API Keys
+# CryptoBot - data understanding historical #
+
+# Import Libraries & API Keys
 from binance.client import Client
 from dotenv import load_dotenv
 import pandas as pd
@@ -13,9 +15,11 @@ binance_api_secret = os.getenv("BINANCE_API_SECRET")
 client = Client(binance_api_key, binance_api_secret, testnet=True)
 
 # Historical Data (Market Data Endpoint) #
+
 # https://binance-docs.github.io/apidocs/spot/en/#market-data-endpoints
 
 # Trade based #
+
 # These data reflect the completed transactions where buyers and sellers have executed their orders on the platform.
 
 # Get trades from Market Data Endpoint
@@ -35,30 +39,14 @@ def build_trad_data_frame(historical_trades):
 
     return df
 
-# print to json file
-def print_to_json_file(filename,df):
-  
-    json_str = df.to_json(orient='records', date_format="iso")  
-
-    with open(filename, "w") as file:
-        file.write(json_str)
-    print("Text file written!", filename)
-
 # run script trades #
 
 # Select parameters
 symbol_array = ["BTCUSDT", "ETHBTC"] 
 limit=1000
 
-path = "data/historical_data/trades"
-os.makedirs(path, exist_ok=True)
-
 for symbol in symbol_array:
-
-    filename = path + '/' + symbol + '.json'
 
     trades = get_hist_trad_data(symbol, limit)
     df = build_trad_data_frame(trades)
-    print_to_json_file(filename, df)
-
-
+    bulk_script.insert_elastic_search(df, symbol)
