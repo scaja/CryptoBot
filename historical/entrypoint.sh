7fe9b7a7542912da8b27ee9742d3d22d77313475
scaja <scajadata@yahoo.com>
2025-03-05 11:06:17 +0100

    comment code

--- historical/entrypoint.sh
+++ historical/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Setze PYTHONPATH dynamisch für Spark
+# Dynamically set the PYTHONPATH for Spark by finding all .zip files in the Spark Python library directory
 export PYTHONPATH=$(find "$SPARK_HOME/python/lib/" -name "*.zip" | tr '\n' ':')
 
-# Starte das Retrieval-Skript
+# Start the retrieval script to fetch and preprocess historical data
 python3 /historical/scripts/retrieval_script.py
+
+# Start the machine learning regression script to train and save the model
 python3 /historical/regression/ml_regression.py
