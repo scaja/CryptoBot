3dfe6ef7e4b95258390b26a1d84255852f09c407
scaja <scajadata@yahoo.com>
2025-04-26 18:25:01 +0200

    start multible scripts with entrypoint script

--- historical/entrypoint.sh
+++ historical/entrypoint.sh
@@ -4,7 +4,15 @@
 export PYTHONPATH=$(find "$SPARK_HOME/python/lib/" -name "*.zip" | tr '\n' ':')
 
 # Start the retrieval script to fetch and preprocess historical data
-python3 /historical/scripts/retrieval_script.py
+python3 /historical/scripts/crypto/retrieval_klines_data.py
+
+python3 /historical/scripts/crypto/retrieval_us_market_data.py
+
+python3 /historical/scripts/crypto/retrieval_ecb_market_data.py
+
+python3 /historical/scripts/crypto/retrieval_macro_economics_data.py
+
+python3 /historical/scripts/crypto/retrieval_order_data.py
 
 # Start the machine learning regression script to train and save the model
-python3 /historical/regression/ml_regression.py
+#python3 /historical/regression/ml_regression.py
